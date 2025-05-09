8259a0df40a226821ddc0a35c168279539203682
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-05-09 09:27:12 -0300

    feat: add loader plugin for css

--- exerciciosjs/webpack/webpack.config.js
+++ exerciciosjs/webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './ex/index.js',
@@ -9,7 +10,11 @@ module.exports = {
     devServer: {
         port: 8080,
         contentBase: './public'
-    },
+    }, 
+    plugins: [
+        new ExtractTextPlugin('app.css')
+    ]
+    ,
     module: {
         loaders: [{
             test: /.js?$/,
@@ -19,6 +24,9 @@ module.exports = {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
+        },{
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
         }]
     }
 }
